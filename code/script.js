197e17b574fd31cb280acdf3a162ea23e790c494
Daniel Stoker <eighty3labels@yahoo.com>
2025-01-14 05:02:49 +0100

    Added comment notes to HTML, JS, CSS, and MD files.

---
+++ code/script.js
@@ -0,0 +1,21 @@
+// Main coding file for project 3, so add and edit as necessary
+
+// Start here
+
+// Step 1 - Welcome and introduction
+// Your code goes here
+alert(
+  `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
+)
+
+// Step 2 - Food choice
+// Your code goes here
+
+// Step 3 - Subtype choice
+// Your code goes here
+
+// Step 4 - Age
+// Your code goes here
+
+// Step 5 - Order confirmation
+// Your code goes here
