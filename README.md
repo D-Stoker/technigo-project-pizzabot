218158bda06abaf2a2d432e0e1380bb0eea1dfd7
Daniel Stoker <eighty3labels@yahoo.com>
2025-01-14 19:30:52 +0100

    Step 1 - welcome and introduction

--- README.md
+++ README.md
@@ -4,6 +4,15 @@ Replace this readme with your own information about the project. You can include
 
 - Brief description of the assignment
 - How you approached the task, what tools and techniques you used, and how you planned it
+
+Step 1: welcome and introduction
+Step 2: food choice
+Step 3: subtype choice
+Step 4: age
+Step 4: order confirmation
+Deployment
+Stretch Goals
+
 - If you had more time, what would be next?
 
 ## View it live
--- code/script.js
+++ code/script.js
@@ -1,12 +1,15 @@
-// Main coding file for project 3, so add and edit as necessary
-
 // Start here
 
 // Step 1 - Welcome and introduction
-// Your code goes here
+
+// 1a - initial welcome message
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
-)
+);
+// 1b - ask for user name
+const userName = prompt("What's your name?");
+// 1c - greet user with their name
+alert(`Hi and welcome, ${userName}!`);
 
 // Step 2 - Food choice
 // Your code goes here
